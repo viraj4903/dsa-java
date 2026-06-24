a3725e5601bcb9eb051f437648102d640e34e5fc
viraj4903 <virajx03@gmail.com>
2026-06-24 23:08:36 +0530

    Created array_v1

---
+++ Arrays/arrays_v1.java
@@ -0,0 +1,17 @@
+public class arrays_v1 {
+    public static void main(String[] args) {
+        
+        int[] arr = new int[5]; 
+
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
